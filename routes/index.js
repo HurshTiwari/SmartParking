d649d50882bc8c084f71d823db5d6297c2354b1b
HurshTiwari <harshdude95@gmail.com>
2016-11-12 12:11:32 +0530

    area selected bug removed

--- routes/index.js
+++ routes/index.js
@@ -247,7 +247,7 @@ module.exports = function(app,passport){
 	    				  return res.json(500,err);
 	    			  }
 	    			  counter++;
-  		    		  if(data.value===false && data.reserved==="0"){
+  		    		  if(data.value===false && spot.reserved==="0"){
   		    			 // console.log('Added spot : '+spot.id);
   		    			  result.push({'spotId': spot.id,'id':spot._id});
   		    		  }
